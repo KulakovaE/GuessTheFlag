0601053514cfc28494bfe9a6439c044e779db08f
Elena Kulakova <egeorgievska89@gmail.com>
2020-01-23 10:06:02 +0100

    Creating the UIView.

---
+++ GuessTheFlag/ButtonForFlagViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ButtonForFlagViewController.swift
+//  GuessTheFlag
+//
+//  Created by Elena Kulakova on 2020-01-23.
+//  Copyright © 2020 Elena Kulakova. All rights reserved.
+//
+
+import UIKit
+
+class ButtonForFlagViewController: UIViewController {
+    
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        askQuestion()
+        borderConfigurationForBtn()
+    }
+    
+    func askQuestion() {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
+    
+    func borderConfigurationForBtn() {
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+    }
+}
